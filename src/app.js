fa38edffe2aef0b60c8dc8949d3d1056712c715c
Matheus <carvalho.bfr@gmail.com>
2020-04-12 21:09:45 +0100

    model usuário com migration funcionando e checando e-mail único

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
-import express from 'express';
-import routes from './routes';
+import express from "express";
+import routes from "./routes";
+
+import "./database";
 
 class App {
-	constructor() {
-		this.server = express();
-		this.routes();
-	}
-
-	middlewares() {
-		this.server.use(express.json());
-	}
-
-	routes() {
-		this.server.use(routes);
-	}
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+  middlewares() {
+    this.server.use(express.json());
+  }
+  routes() {
+    this.server.use(routes);
+  }
 }
-
 export default new App().server;
